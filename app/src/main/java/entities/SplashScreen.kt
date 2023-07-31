a67359eedc88a09e950c2c8a2045f1d3d0e36c22
briayysha <browndenyka@yahoo.com>
2023-07-31 17:41:27 -0500

    splashscreen

---
+++ app/src/main/java/entities/SplashScreen.kt
@@ -0,0 +1,12 @@
+package entities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.plantage2.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
